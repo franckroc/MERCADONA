c58f699a743ad3696fb841133a8c5f3ce1e9b937
Franck Rochette <rochette.franck@gmail.com>
2023-05-08 11:09:31 +0200

    add <a> page up

--- test_start_app.py
+++ test_start_app.py
@@ -7,7 +7,7 @@ def root_test():
 
     print("Test app")
     response = client.get("/")
-    print("Status response: ",response.status_code)
+    print("Status response Page d'accueil: ",response.status_code)
     assert response.status_code == 200
 
 root_test()
